c9ac60eff82686dcec52ecb44d440a3b6440a0d1
Odeinjul <odeinjul@gmail.com>
2023-11-30 14:09:24 +0800

    Update t4_graphsage_acc_papers100m.sh

--- t4_graphsage_acc_papers100m.sh
+++ t4_graphsage_acc_papers100m.sh
@@ -1,5 +1,5 @@
 python3 ~/workspace/dgl/tools/launch.py --workspace ~/workspace/dgl/examples/pytorch/graphsage/dist/ \
- --num_trainers 8 --num_servers 1  --num_samplers 1  --part_config /home/ubuntu/workspace/data/ogbn-papers100m_4p/ogb-paper100M.json \
+ --num_trainers 8 --num_servers 1  --num_samplers 1  --part_config /home/ubuntu/workspace/data/ogbn-papers100m_4p_ud/ogb-paper100M.json \
    --ip_config ip_config_4p.txt   \
    "/home/ubuntu/anaconda3/envs/dglbase/bin/python3 train_dist_transductive_1129.py --graph_name ogb-paper100M \
    --ip_config ip_config_4p.txt --batch_size 1000 --num_gpus 8 --eval_every 5 --num_epochs 20 \
